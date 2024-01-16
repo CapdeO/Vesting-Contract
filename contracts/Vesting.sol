dbd93915b9e6868a142f19dd8ec42a75314441ba
Capde <capde22@gmail.com>
2024-01-16 17:52:30 -0300

    a

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,10 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Vesting {
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract Vesting is Initializable, ERC20Upgradeable, ERC20PausableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+
+
 
     constructor()  {
 
     }
 
+    function buyTokens(address _stableAddress, uint256 _stableAmount) public {
+        
+        uint256 time = block.timestamp;
+
+
+
+
+    }
 }
