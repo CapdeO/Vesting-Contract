3838ac70d342dbc384a756d8fcd937e537b19b6b
Capde <capde22@gmail.com>
2024-01-25 17:35:16 -0300

    fix date

--- test/Vesting.js
+++ test/Vesting.js
@@ -3,15 +3,15 @@ var { expect } = require("chai");
 var { ethers, upgrades } = require("hardhat");
 var { time } = require("@nomicfoundation/hardhat-network-helpers");
 
-const phase0 = [1706745600, 1709251199, 0, ethers.parseEther("0.04"),   ethers.parseEther("8000000"),  ethers.parseEther("400000")]
-const phase1 = [1709251200, 1711929599, 0, ethers.parseEther("0.06"),  ethers.parseEther("32000000"), ethers.parseEther("1600000")]
-const phase2 = [1711929600, 1714521599, 0, ethers.parseEther("0.07"),   ethers.parseEther("6000000"),  ethers.parseEther("300000")]
-const phase3 = [1714521600, 1717199999, 0, ethers.parseEther("0.075"), ethers.parseEther("40000000"), ethers.parseEther("2000000")]
-const phase4 = [1717200000, 1719791999, 0, ethers.parseEther("0.08"),  ethers.parseEther("40000000"), ethers.parseEther("2000000")]
-const phase5 = [1719792000, 1722470399, 0, ethers.parseEther("0.085"), ethers.parseEther("60000000"), ethers.parseEther("3000000")]
-const phase6 = [1722470400, 1725148799, 0, ethers.parseEther("0.09"),  ethers.parseEther("64000000"), ethers.parseEther("3200000")]
-const phase7 = [1725148800, 1727740799, 0, ethers.parseEther("0.095"), ethers.parseEther("70000000"), ethers.parseEther("3500000")]
-const vestingEnd = 1772323199 // Saturday, 28 February 2026 23:59:59
+const phase0 = [1709251200, 1711929599, 0, ethers.parseEther("0.04"),   ethers.parseEther("8000000"),  ethers.parseEther("400000")]
+const phase1 = [1711929600, 1714521599, 0, ethers.parseEther("0.06"),  ethers.parseEther("32000000"), ethers.parseEther("1600000")]
+const phase2 = [1714521600, 1717199999, 0, ethers.parseEther("0.07"),   ethers.parseEther("6000000"),  ethers.parseEther("300000")]
+const phase3 = [1717200000, 1719791999, 0, ethers.parseEther("0.075"), ethers.parseEther("40000000"), ethers.parseEther("2000000")]
+const phase4 = [1719792000, 1722470399, 0, ethers.parseEther("0.08"),  ethers.parseEther("40000000"), ethers.parseEther("2000000")]
+const phase5 = [1722470400, 1725148799, 0, ethers.parseEther("0.085"), ethers.parseEther("60000000"), ethers.parseEther("3000000")]
+const phase6 = [1725148800, 1727740799, 0, ethers.parseEther("0.09"),  ethers.parseEther("64000000"), ethers.parseEther("3200000")]
+const phase7 = [1727740800, 1730419199, 0, ethers.parseEther("0.095"), ethers.parseEther("70000000"), ethers.parseEther("3500000")]
+const vestingEnd = 1775001600 // Wednesday, 1 April 2026 0:00:00
 const oneMonth = 2629743 // In secs
 
 describe("Vesting Contract", () => {
