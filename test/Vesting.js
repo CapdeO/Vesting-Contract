417cc3df00f5cebc2f46d52a68a2d716343d82c3
Cap <capde22@gmail.com>
2024-02-01 10:11:02 -0300

    a little more testing

--- test/Vesting.js
+++ test/Vesting.js
@@ -57,91 +57,111 @@ describe("Vesting Contract", () => {
             await vesting.createPhase(...phase0)
 
             expect((await vesting.getPhases.call())[0].startTime).to.be.equal(phase0[0])
-            expect(await dj.balanceOf(vesting)).to.be.equal(amount)
-        });
-
-        it("Get actual Phase", async () => {
-            var { vesting, dj } = await loadFixture(loadTest);
-
-            await expect(vesting.getCurrentPhaseNumber()).to.be.revertedWith("No vesting phases available.")
-
-            await vesting.setVestingParams(vestingEnd, oneMonth)
-            await dj.approve(vesting, phase0[4])
-            await vesting.createPhase(...phase0)
-
-            await dj.approve(vesting, phase1[4])
-            await vesting.createPhase(...phase1)
-
-            await dj.approve(vesting, phase2[4])
-            await vesting.createPhase(...phase2)
-
-            await expect(vesting.getCurrentPhaseNumber()).to.be.revertedWith("No active vesting phase for the current time.")
-
-            await time.increaseTo(phase0[0])
-
-            expect(await vesting.getCurrentPhaseNumber()).to.be.equal(0)
-
-            await time.increaseTo(phase1[0])
-
-            expect(await vesting.getCurrentPhaseNumber()).to.be.equal(1)
-
-            await time.increaseTo(phase2[1] + 1)
-
-            await expect(vesting.getCurrentPhaseNumber()).to.be.revertedWith("No active vesting phase for the current time.")
-
-            await expect(vesting.getPhase(3)).to.be.revertedWith("Invalid phase number.")
+            expect(await dj.balanceOf(vesting)).to.equal(amount)
         });
     });
 
     describe("Invest", () => {
-        it("Invest in phase", async () => {
+        it("Invest", async () => {
             var { vesting, usdt, dj, alice, race } = await loadFixture(loadTest);
+            let aliceInvestment;
 
             await vesting.setVestingParams(vestingEnd, oneMonth)
-            await dj.approve(vesting, phase0[4])
+            await dj.approve(vesting, ethers.parseEther("320000000"))
+            await usdt.mint(alice, ethers.parseUnits("100", 6))
+            await usdt.connect(alice).approve(vesting, ethers.parseUnits("100", 6))
+
             await vesting.createPhase(...phase0)
-
-            await dj.approve(vesting, phase1[4])
             await vesting.createPhase(...phase1)
+            await vesting.createPhase(...phase2)
+            await vesting.createPhase(...phase3)
+            await vesting.createPhase(...phase4)
+            await vesting.createPhase(...phase5)
+            await vesting.createPhase(...phase6)
+            await vesting.createPhase(...phase7)
 
-            let amount = 1000000 // ONE DOLLAR
             await time.increaseTo(1711463881) //  Tuesday, 26 March 2024 14:38:01
-            let currentPhaseNumber = await vesting.getCurrentPhaseNumber()
-            let tokensToReceive = ethers.parseEther("25")
-
-            await usdt.mint(alice, amount)
-            await usdt.connect(alice).approve(vesting, amount)
-            await expect(vesting.connect(alice).invest(usdt.target, amount))
-                .to.emit(vesting, "BuyTokens").withArgs(currentPhaseNumber, alice.address, ethers.parseEther("1"), tokensToReceive)
-            
-            expect(await usdt.balanceOf(alice)).to.be.equal(0)
-            expect(await usdt.balanceOf(race)).to.be.equal(amount)
-
-            let aliceInvestment = await vesting.getUserInvestment(alice.address, currentPhaseNumber)
-
-            expect(aliceInvestment[0]).to.be.equal(tokensToReceive)
-            expect(aliceInvestment[1]).to.be.equal(tokensToReceive)
-            expect(aliceInvestment[2]).to.be.equal(0)
+            await expect(vesting.connect(alice).invest(usdt.target, ethers.parseUnits("1", 6)))
+                .to.emit(vesting, "BuyTokens").withArgs(0, alice.address, ethers.parseEther("1"), ethers.parseEther("25"))
+            expect(await usdt.balanceOf(alice)).to.equal(ethers.parseUnits("99", 6))
+            expect(await usdt.balanceOf(race)).to.equal(ethers.parseUnits("1",6))
+            aliceInvestment = await vesting.getUserInvestment(alice.address, 0)
+            expect(aliceInvestment[0]).to.equal(ethers.parseEther("25"))
+            expect(aliceInvestment[1]).to.equal(ethers.parseEther("25"))
+            expect(aliceInvestment[2]).to.equal(0)
 
             await time.increaseTo(1714142281) //  Friday, 26 April 2024 14:38:01
-            currentPhaseNumber = await vesting.getCurrentPhaseNumber()
+            await expect(vesting.connect(alice).invest(usdt.target, ethers.parseUnits("1", 6)))
+                .to.emit(vesting, "BuyTokens").withArgs(1, alice.address, ethers.parseEther("1"), "16666666666666666666")
+            expect(await usdt.balanceOf(alice)).to.equal(ethers.parseUnits("98", 6))
+            expect(await usdt.balanceOf(race)).to.equal(ethers.parseUnits("2", 6))
+            aliceInvestment = await vesting.getUserInvestment(alice.address, 1)
+            expect(aliceInvestment[0]).to.equal("16666666666666666666")
+            expect(aliceInvestment[1]).to.equal("16666666666666666666")
+            expect(aliceInvestment[2]).to.equal(0)
 
-            amount = 300000000 // 300 DOLLARS
-            tokensToReceive = ethers.parseEther("5000") 
+            await time.increaseTo(1714563849) //  Wednesday, 1 May 2024 11:44:09
+            await expect(vesting.connect(alice).invest(usdt.target, ethers.parseUnits("1", 6)))
+                .to.emit(vesting, "BuyTokens").withArgs(2, alice.address, ethers.parseEther("1"), "14285714285714285714")
+            expect(await usdt.balanceOf(alice)).to.equal(ethers.parseUnits("97", 6))
+            expect(await usdt.balanceOf(race)).to.equal(ethers.parseUnits("3", 6))
+            aliceInvestment = await vesting.getUserInvestment(alice.address, 2)
+            expect(aliceInvestment[0]).to.equal("14285714285714285714")
+            expect(aliceInvestment[1]).to.equal("14285714285714285714")
+            expect(aliceInvestment[2]).to.equal(0)
 
-            await usdt.mint(alice, amount)
-            await usdt.connect(alice).approve(vesting, amount)
+            await time.increaseTo(1717242249) //  Saturday, 1 June 2024 11:44:09
+            await expect(vesting.connect(alice).invest(usdt.target, ethers.parseUnits("1", 6)))
+                .to.emit(vesting, "BuyTokens").withArgs(3, alice.address, ethers.parseEther("1"), "13333333333333333333")
+            expect(await usdt.balanceOf(alice)).to.equal(ethers.parseUnits("96", 6))
+            expect(await usdt.balanceOf(race)).to.equal(ethers.parseUnits("4", 6))
+            aliceInvestment = await vesting.getUserInvestment(alice.address, 3)
+            expect(aliceInvestment[0]).to.equal("13333333333333333333")
+            expect(aliceInvestment[1]).to.equal("13333333333333333333")
+            expect(aliceInvestment[2]).to.equal(0)
 
-            await expect(vesting.connect(alice).invest(usdt.target, amount))
-                .to.emit(vesting, "BuyTokens").withArgs(currentPhaseNumber, alice.address, ethers.parseEther("300"), tokensToReceive)
+            await time.increaseTo(1719834249) //  Monday, 1 July 2024 11:44:09
+            await expect(vesting.connect(alice).invest(usdt.target, ethers.parseUnits("1", 6)))
+                .to.emit(vesting, "BuyTokens").withArgs(4, alice.address, ethers.parseEther("1"), ethers.parseEther("12.5"))
+            expect(await usdt.balanceOf(alice)).to.equal(ethers.parseUnits("95", 6))
+            expect(await usdt.balanceOf(race)).to.equal(ethers.parseUnits("5", 6))
+            aliceInvestment = await vesting.getUserInvestment(alice.address, 4)
+            expect(aliceInvestment[0]).to.equal(ethers.parseEther("12.5"))
+            expect(aliceInvestment[1]).to.equal(ethers.parseEther("12.5"))
+            expect(aliceInvestment[2]).to.equal(0)
 
-            aliceInvestment = await vesting.getUserInvestment(alice.address, currentPhaseNumber)
+            await time.increaseTo(1722512649) //  Thursday, 1 August 2024 11:44:09
+            await expect(vesting.connect(alice).invest(usdt.target, ethers.parseUnits("1", 6)))
+                .to.emit(vesting, "BuyTokens").withArgs(5, alice.address, ethers.parseEther("1"), "11764705882352941176")
+            expect(await usdt.balanceOf(alice)).to.equal(ethers.parseUnits("94", 6))
+            expect(await usdt.balanceOf(race)).to.equal(ethers.parseUnits("6", 6))
+            aliceInvestment = await vesting.getUserInvestment(alice.address, 5)
+            expect(aliceInvestment[0]).to.equal("11764705882352941176")
+            expect(aliceInvestment[1]).to.equal("11764705882352941176")
+            expect(aliceInvestment[2]).to.equal(0)
 
-            expect(aliceInvestment[0]).to.be.equal(tokensToReceive)
-            expect(aliceInvestment[1]).to.be.equal(tokensToReceive)
-            expect(aliceInvestment[2]).to.be.equal(0)
+            await time.increaseTo(1725191049) //  Sunday, 1 September 2024 11:44:09
+            await expect(vesting.connect(alice).invest(usdt.target, ethers.parseUnits("1", 6)))
+                .to.emit(vesting, "BuyTokens").withArgs(6, alice.address, ethers.parseEther("1"), "11111111111111111111")
+            expect(await usdt.balanceOf(alice)).to.equal(ethers.parseUnits("93", 6))
+            expect(await usdt.balanceOf(race)).to.equal(ethers.parseUnits("7", 6))
+            aliceInvestment = await vesting.getUserInvestment(alice.address, 6)
+            expect(aliceInvestment[0]).to.equal("11111111111111111111")
+            expect(aliceInvestment[1]).to.equal("11111111111111111111")
+            expect(aliceInvestment[2]).to.equal(0)
 
-            expect(await usdt.balanceOf(race)).to.be.equal(1000000 + 300000000)
+            await time.increaseTo(1727783049) //  Tuesday, 1 October 2024 11:44:09
+            await expect(vesting.connect(alice).invest(usdt.target, ethers.parseUnits("1", 6)))
+                .to.emit(vesting, "BuyTokens").withArgs(7, alice.address, ethers.parseEther("1"), "10526315789473684210")
+            expect(await usdt.balanceOf(alice)).to.equal(ethers.parseUnits("92", 6))
+            expect(await usdt.balanceOf(race)).to.equal(ethers.parseUnits("8", 6))
+            aliceInvestment = await vesting.getUserInvestment(alice.address, 7)
+            expect(aliceInvestment[0]).to.equal("10526315789473684210")
+            expect(aliceInvestment[1]).to.equal("10526315789473684210")
+            expect(aliceInvestment[2]).to.equal(0)
+
+            await time.increaseTo(phase7[1] + 1) //  Friday, 1 November 2024 0:00:00
+            await expect(vesting.connect(alice).invest(usdt.target, ethers.parseUnits("1", 6))).to.be.revertedWith("No active vesting phase for the current time.")
         });
     });
 
@@ -179,10 +199,7 @@ describe("Vesting Contract", () => {
             await vesting.connect(alice).invest(usdt.target, 4000000)
 
             await time.increaseTo(1713190474)
-
         });
-
-
     });
 
     describe("Read functions", () => {
@@ -200,8 +217,64 @@ describe("Vesting Contract", () => {
             expect(await vesting.getPhasesCount()).to.be.equal(2)
         });
 
+        it("Get actual Phase", async () => {
+            var { vesting, dj } = await loadFixture(loadTest);
+
+            await expect(vesting.getCurrentPhaseNumber()).to.be.revertedWith("No vesting phases available.")
+            await dj.approve(vesting, ethers.parseEther("320000000"))
+
+            await vesting.setVestingParams(vestingEnd, oneMonth)
+            await vesting.createPhase(...phase0)
+            await vesting.createPhase(...phase1)
+            await vesting.createPhase(...phase2)
+            await vesting.createPhase(...phase3)
+            await vesting.createPhase(...phase4)
+            await vesting.createPhase(...phase5)
+            await vesting.createPhase(...phase6)
+            await vesting.createPhase(...phase7)
+
+            await expect(vesting.getCurrentPhaseNumber()).to.be.revertedWith("No active vesting phase for the current time.")
+
+            await time.increaseTo(phase0[0])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(0)
+            await time.increaseTo(phase0[1])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(0)
+            await time.increaseTo(phase1[0])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(1)
+            await time.increaseTo(phase1[1])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(1)
+            await time.increaseTo(phase2[0])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(2)
+            await time.increaseTo(phase2[1])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(2)
+            await time.increaseTo(phase3[0])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(3)
+            await time.increaseTo(phase3[1])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(3)
+            await time.increaseTo(phase4[0])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(4)
+            await time.increaseTo(phase4[1])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(4)
+            await time.increaseTo(phase5[0])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(5)
+            await time.increaseTo(phase5[1])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(5)
+            await time.increaseTo(phase6[0])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(6)
+            await time.increaseTo(phase6[1])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(6)
+            await time.increaseTo(phase7[0])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(7)
+            await time.increaseTo(phase7[1])
+            expect(await vesting.getCurrentPhaseNumber()).to.equal(7)
+
+            await time.increaseTo(phase7[1] + 1)
+            await expect(vesting.getCurrentPhaseNumber()).to.revertedWith("No active vesting phase for the current time.")
+            await expect(vesting.getPhase(8)).to.be.revertedWith("Invalid phase number.")
+        });
+
         it("getUserInvestments", async () => {
-            var { vesting, usdt, dj, alice, race } = await loadFixture(loadTest);
+            var { vesting, usdt, dj, alice } = await loadFixture(loadTest);
 
             await vesting.setVestingParams(vestingEnd, oneMonth)
 
@@ -224,23 +297,22 @@ describe("Vesting Contract", () => {
 
             let aliceInvestments = await vesting.getUserInvestments(alice.address)
 
-            expect(aliceInvestments[0][0]).to.be.equal(ethers.parseEther("12500"))
-            expect(aliceInvestments[0][1]).to.be.equal(ethers.parseEther("12500"))
-            expect(aliceInvestments[0][2]).to.be.equal(0)
-            expect(aliceInvestments[1][0]).to.be.equal(ethers.parseEther("5000"))
-            expect(aliceInvestments[1][1]).to.be.equal(ethers.parseEther("5000"))
-            expect(aliceInvestments[1][2]).to.be.equal(0)
-            expect(aliceInvestments[2][0]).to.be.equal("2857142857142857142857")
-            expect(aliceInvestments[2][1]).to.be.equal("2857142857142857142857")
-            expect(aliceInvestments[2][2]).to.be.equal(0)
-
-            // console.log(aliceInvestments[0][0])
+            expect(aliceInvestments[0][0]).to.equal(ethers.parseEther("12500"))
+            expect(aliceInvestments[0][1]).to.equal(ethers.parseEther("12500"))
+            expect(aliceInvestments[0][2]).to.equal(0)
+            expect(aliceInvestments[1][0]).to.equal(ethers.parseEther("5000"))
+            expect(aliceInvestments[1][1]).to.equal(ethers.parseEther("5000"))
+            expect(aliceInvestments[1][2]).to.equal(0)
+            expect(aliceInvestments[2][0]).to.be.at.least(ethers.parseEther((200/0.07).toString()))
+            expect(aliceInvestments[2][1]).to.be.at.least(ethers.parseEther((200/0.07).toString()))
+            expect(aliceInvestments[2][2]).to.equal(0)
 
             await vesting.connect(alice).release(0)
+            aliceInvestments = await vesting.getUserInvestments(alice.address)
 
-            // console.log(await vesting.getUserInvestments(alice.address))
+            expect(aliceInvestments[0][0]).to.equal(ethers.parseEther("12500"))
+            expect(aliceInvestments[0][1]).to.be.at.least(ethers.parseEther("11950"))
+            expect(aliceInvestments[0][2]).to.be.at.least(ethers.parseEther("520"))
         });
-
-        
     });
 });
