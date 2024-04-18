1b869bf5ca7b3fbfb0d385b038a19ba072138a2e
Cap <capde22@gmail.com>
2024-04-18 10:28:13 -0300

    amoy vesting phase

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,18 +1,22 @@
 const { ethers, upgrades } = require("hardhat");
 
-// Mumbai
-// 0x7a6C7a3bab11D57423f9F5690AF6ff38BE2d771f USDT
-// 0xD345D94552C88DD997F117C5987657526616ECB6 DJ
-// 0x8Db673441f806aD20526d8Bb30Af54F03B798F82 Vesting proxy
-// 0xDa005Ae70DCf099191de08fb0dE1bf5FD2c4ED50 Vesting Impl v1
+// Amoy
+// 0x20cfc5962b1abe4931688ff71de3d5ee053fb283 USDT
+// 0xD1e8372C333158331eDf0edd994270AD8896E0a3 USDC
+// 0x755B7bd472Eb98cb355A6e9c1588c03c19141471 BUSD
+// 0xf88718d191892cde8774dccebc12a024289d96ea DJ
+// 0x29c55A22976288B226c8D55b2E9fa411EB4657C3 Vesting proxy
+// 0x94D323fd50590C49dBbBc563Ca8E4fe331b27ea7 Vesting impl
 
 // Polygon
 //
 //
 
-// Test phases
-const firstPhase = [1706554332, 1709251199, 0, ethers.parseEther("0.04"),   ethers.parseEther("8000000"),  ethers.parseEther("400000")]
-const phase0 = [1709251200, 1711929599, 0, ethers.parseEther("0.04"),   ethers.parseEther("8000000"),  ethers.parseEther("400000")]
+// TEST PARAMS
+const vestingEnd = 1776557047 // Sunday, 19 April 2026 0:04:07
+const oneMonth = 2629743
+
+const firstPhase = [1711929600, 1714521599, 0, ethers.parseEther("0.04"),   ethers.parseEther("8000000"),  ethers.parseEther("400000")]
 
 const wait = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -20,33 +24,43 @@ const wait = (ms) => {
 
 async function main() {
 
-    const Vesting = await hre.ethers.getContractFactory("Vesting");
-    const vesting = Vesting.attach('0x8Db673441f806aD20526d8Bb30Af54F03B798F82');
+    const vesting = await hre.ethers.getContractAt(
+        "Vesting",
+        "0x29c55A22976288B226c8D55b2E9fa411EB4657C3"
+    )
 
-    const DreamJunk = await hre.ethers.getContractFactory("DreamJunk");
-    const dreamJunk = DreamJunk.attach('0xD345D94552C88DD997F117C5987657526616ECB6');
+    const dreamJunk = await hre.ethers.getContractAt(
+        "DreamJunkStudios",
+        "0xf88718d191892cde8774dccebc12a024289d96ea"
+    )
 
     // console.log('Establishing vesting parameters...')
     // const setVestingParamsTransaction = await vesting.setVestingParams(
-    //     1769711624, // Thursday, 29 January 2026 18:33:44
-    //     2629743
+    //     vestingEnd,
+    //     oneMonth
     // );
     // await setVestingParamsTransaction.wait()
     // await wait(30000);
     // console.log('Vesting parameters ready.')
 
-    console.log('Approving DJ...')
-    const approveTransaction = await dreamJunk.approve(vesting.target, firstPhase[4])
-    await approveTransaction.wait()
-    await wait(30000)
-    console.log('Approval ready.')
+    // const end = await vesting.vestingEnd()
+    // console.log(end)
 
-    await wait(20000);
-    console.log('Creating phase...')
-    const createPhaseTransaction = await vesting.createPhase(...firstPhase);
-    await createPhaseTransaction.wait()
-    await wait(30000)
-    console.log('Phase created.')
+    // console.log('Approving DJ...')
+    // const approveTransaction = await dreamJunk.approve(vesting.target, firstPhase[4])
+    // await approveTransaction.wait()
+    // await wait(30000)
+    // console.log('Approval ready.')
+
+    // await wait(20000);
+    // console.log('Creating phase...')
+    // const createPhaseTransaction = await vesting.createPhase(...firstPhase);
+    // await createPhaseTransaction.wait()
+    // await wait(30000)
+    // console.log('Phase created.')
+
+    const end = await vesting.getPhase(0)
+    console.log(end)
 }
 
 
